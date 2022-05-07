77c09aeaa9b575cc81e0b9411a009cfaa5ef9cf9
Roman <rpol123453@gmail.com>
2022-05-07 17:34:12 +0300

    Add constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,14 @@
+export const ROW = 2;
+export const COLUMN = 3;
+export const UNIT_NAMES = [
+  'Skeleton',
+  'Skeleton Mage',
+  'Centaur',
+  'Bandit',
+  'Archimage',
+  'Elf Archer',
+  'Bishop',
+  'Monk',
+  'Sirena',
+];
+
