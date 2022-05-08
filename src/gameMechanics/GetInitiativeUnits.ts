d11ead8c364dcb4ab73c020e288471264efbf3ea
Roman <rpol123453@gmail.com>
2022-05-08 23:53:21 +0300

    Add getInitiative function

---
+++ src/gameMechanics/GetInitiativeUnits.ts
@@ -0,0 +1,18 @@
+import { Unit } from '../units/Unit';
+import { TurnedUnits } from './TurnedUnits';
+
+export const GetInitiativeUnits = (
+  allUnits: Unit[],
+  turnedUnits: TurnedUnits
+): Unit | null => {
+  for (let index = 0; index < allUnits.length; index++) {
+    if (
+      allUnits[index].healthPoint > 0 &&
+      !turnedUnits.turnedUnits.includes(allUnits[index])
+    ) {
+      return allUnits[index];
+    }
+  }
+
+  return null;
+};
