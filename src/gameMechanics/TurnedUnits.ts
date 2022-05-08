52404966a3733125eef2c0b947bc861ac80e831f
Roman <rpol123453@gmail.com>
2022-05-08 23:56:04 +0300

    Create TurnedUnits class

---
+++ src/gameMechanics/TurnedUnits.ts
@@ -0,0 +1,17 @@
+import { Unit } from '../units/Unit';
+
+export class TurnedUnits {
+  private _turnedUnits: Unit[];
+
+  constructor(turnedUnits: Unit[]) {
+    this._turnedUnits = turnedUnits;
+  }
+
+  public get turnedUnits(): Unit[] {
+    return this._turnedUnits;
+  }
+
+  public set turnedUnits(value: Unit[]) {
+    this._turnedUnits = value;
+  }
+}
