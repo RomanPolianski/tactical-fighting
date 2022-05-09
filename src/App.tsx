7bc6c35f426557515f826e919090c47359351ad4
Roman <rpol123453@gmail.com>
2022-05-09 18:11:44 +0300

    Add Table component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import './App.css';
+import { Field } from './components/Field/Field';
+import { combineUnits } from './gameMechanics/CombineUnits';
+import { createTeam } from './gameMechanics/CreateTeam';
+import { getTurnOrderUnitsTeam } from './gameMechanics/GetTurnOrderUnitsTeam';
+import { Round } from './gameMechanics/Round';
+import { TurnedUnits } from './gameMechanics/TurnedUnits';
 
 function App() {
+  const round = new Round();
+  const turnedUnits = new TurnedUnits([]);
+  const { firstTeam, secondTeam } = createTeam();
+  const firstTeamOrder = getTurnOrderUnitsTeam(firstTeam);
+  const secondTeamOrder = getTurnOrderUnitsTeam(secondTeam);
+  const combineOrder = combineUnits(firstTeamOrder, secondTeamOrder);
 
-  return <div className="App">hello</div>;
+  return (
+    <Field
+      round={round}
+      roundTurnedUnits={turnedUnits}
+      firstTeam={firstTeam}
+      secondTeam={secondTeam}
+      firstTeamOrder={firstTeamOrder}
+      secondTeamOrder={secondTeamOrder}
+      combineOrder={combineOrder}
+    ></Field>
+  );
 }
 
 export default App;
